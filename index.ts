e1f2d78718af7c2b98535c11626bc4d2a7fe9767
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-05-12 16:29:50 +0700

    Update files

--- index.ts
+++ index.ts
@@ -1,28 +1,28 @@
-import { RefreshingAuthProvider } from "@twurple/auth"
-import { ChatClient } from "@twurple/chat"
-import { promises as fs } from "fs"
-import dotenv from "dotenv-flow"
+import { RefreshingAuthProvider } from "@twurple/auth";
+import { ChatClient } from "@twurple/chat";
+import { promises as fs } from "fs";
+import dotenv from "dotenv-flow";
 
 dotenv.config({
   default_node_env: "development",
-})
+});
 
-const investIntervalMs = 303000
-const farmIntervalMs = 301000
-const stopTime = 2 * 60 * 60 * 1000
-let running = true
+const investIntervalMs = 303000;
+const farmIntervalMs = 301000;
+const stopTime = 2 * 60 * 60 * 1000;
+let running = true;
 
-const clientId = process.env.CLIENT_ID as string
-const clientSecret = process.env.CLIENT_SECRET as string
-const channel = process.env.CHANNEL as string
+const clientId = process.env.CLIENT_ID as string;
+const clientSecret = process.env.CLIENT_SECRET as string;
+const channel = process.env.CHANNEL as string;
 
 async function main() {
   setTimeout(() => {
-    console.log("Stopped")
-    running = false
-  }, stopTime)
+    console.log("Stopped");
+    running = false;
+  }, stopTime);
 
-  const tokenData = JSON.parse(await fs.readFile("./tokens.json", "utf-8"))
+  const tokenData = JSON.parse(await fs.readFile("./tokens.json", "utf-8"));
   const auth = new RefreshingAuthProvider(
     {
       clientId,
@@ -35,57 +35,53 @@ async function main() {
         ),
     },
     tokenData
-  )
+  );
 
   function randomIntFromInterval() {
-    return Math.floor(Math.random() * (5 - 1 + 1) + 1)
+    return Math.floor(Math.random() * (5 - 1 + 1) + 1);
   }
 
-  const chatClient = new ChatClient(auth, { channels: [channel] })
+  const chatClient = new ChatClient(auth, { channels: [channel] });
 
   chatClient.onConnect(async () => {
-    console.log("CONNECTED", { channel })
-
+    console.log("CONNECTED", { channel });
 
     setInterval(async () => {
       if (!running) {
-        return
+        return;
       }
 
-      const amounts = randomIntFromInterval()
-console.log(amounts)
-      const invest = `!invest narzelAmogus`
+      const amounts = randomIntFromInterval();
+      console.log(amounts);
+      const invest = `!invest narzelAmogus`;
       await chatClient.say(channel, invest).then(
         () => {
-          console.log("Sent", { invest } )
+          console.log("Sent", { invest });
         },
         (reason) => {
-          console.error("Not sent", { reason })
+          console.error("Not sent", { reason });
         }
-        )
-
-      }, investIntervalMs)
-
+      );
+    }, investIntervalMs);
 
     setInterval(async () => {
       if (!running) {
-        return
+        return;
       }
 
-      const message = `!farm`
+      const message = `!farm`;
       await chatClient.say(channel, message).then(
         () => {
-          console.log("Sent", { message } )
+          console.log("Sent", { message });
         },
         (reason) => {
-          console.error("Not sent", { reason })
+          console.error("Not sent", { reason });
         }
-      )
+      );
+    }, farmIntervalMs);
+  });
 
-    }, farmIntervalMs)
-  })
-
-  await chatClient.connect()
+  await chatClient.connect();
 }
 
-main()
+main();
